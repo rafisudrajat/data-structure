4e7ea8470b65d48bf261e669113c116b7e59554e
rafisudrajat <rafisudrajat633@yahoo.com>
2023-11-16 18:54:25 +0900

    Add Insertion sort implementation

---
+++ Lecture_3/InsertionSort.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+void swapArrayElement(int* array,int leftIndex, int rightIndex){
+    if(leftIndex!=rightIndex){
+        array[leftIndex] += array[rightIndex];
+        array[rightIndex] = array[leftIndex] - array[rightIndex];
+        array[leftIndex] -= array[rightIndex];
+    }
+}
+
+void sortSubArray(int *array, int currentIndex){
+    int arrayElement=array[currentIndex];
+    while (currentIndex>0 && array[currentIndex-1]>arrayElement)
+    {
+        swapArrayElement(array,currentIndex-1,currentIndex);
+        currentIndex--;
+    }   
+}
+
+void insertionSort(int* array,int arraySize){
+    for(int i=0;i<arraySize;i++){
+        sortSubArray(array,i);
+    }
+}
+
+void printArray(int* array,int size){
+    for(int i=0;i<size;i++){
+        std::cout<<array[i]<<" ";
+    }
+    std::cout<<"\n";
+}
+
+int main(){
+    int arrayTest[4]={0,4,2,1};
+    int secondArrayTest[5]={0,9,7,8,2};
+    insertionSort(arrayTest,4);
+    insertionSort(secondArrayTest,5);
+    printArray(arrayTest,4);
+    printArray(secondArrayTest,5);
+}
